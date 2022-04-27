f0dd61a0db436155a024148647a9150f009ad6f6
Nathanael Ang <natnatang@hotmail.com>
2022-04-27 19:08:04 +1000

    Updated Logo

--- src/Maze/Logo.java
+++ src/Maze/Logo.java
@@ -5,30 +5,21 @@ import java.util.ArrayList;
 public class Logo {
 
     private int dimensions;
-    private ArrayList<Object> shape;
-    private ArrayList<Object> image;
+    private String image;
 
-    public ArrayList<Object> GetImage(){
-        return image;
-    }
-
-    public ArrayList<Object>  GetShape(){
-       return shape;
-    }
-
-    public void SetImage(ArrayList<Object> image){
-        this.image = image;
-    }
-
-    public void SetShape(ArrayList<Object> shape){
-        this.shape = shape;
-    }
-
-    public int GetSize() {
+    public int getDimensions() {
         return dimensions;
     }
 
-    public void SetSize(int dimensions){
-       this.dimensions = dimensions;
+    public void setDimensions(int dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
