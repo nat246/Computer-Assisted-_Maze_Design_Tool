7f00d6e5eb9412d498625f43ebc387b9def0a6b2
Li <35750523+vCel@users.noreply.github.com>
2022-04-27 21:57:29 +1000

    Added Cell type to Cell, removed UI Handler

--- src/Maze/Cell.java
+++ src/Maze/Cell.java
@@ -10,10 +10,12 @@ import java.awt.event.MouseAdapter;
 public class Cell extends Grid {
     
     private HashMap<String, Boolean> walls;
+    private int cellType; // 0 is normal cell, 1 is start point, 2 is end point
 
     public Cell(int x, int y) {
         super(x, y);
         initWalls();
+        cellType = 0;
     }
 
     public Boolean wallStatus(String location) {
@@ -23,6 +25,15 @@ public class Cell extends Grid {
     public void setWall(String wall, boolean state) {
         walls.replace(wall, state);
     }
+
+    public void setType(int type) {
+        this.cellType = type;
+    }
+
+    public int getType() {
+        return this.cellType;
+    }
+
 
     private void initWalls() {
         walls = new HashMap<>();
