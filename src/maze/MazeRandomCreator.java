41ca5bb37a58cf61cb9ad878335c67c81e6da16a
Nathanael Ang <natnatang@hotmail.com>
2022-05-18 17:51:43 +1000

    Update MazeRandomCreator.java

--- src/maze/MazeRandomCreator.java
+++ src/maze/MazeRandomCreator.java
@@ -1,8 +1,15 @@
 package maze;
+
+import java.util.ArrayList;
+
 /**
  * used for creating a random generated maze
  */
 public class MazeRandomCreator {
+    private int mazeWidth;
+    private int mazeHeight;
+    private int visitedCells;
+    private ArrayList<ArrayList<Integer>> pathStack = new ArrayList<ArrayList<Integer>>();
 
 
 }
