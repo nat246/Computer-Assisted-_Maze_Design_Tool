4c1d9f5c098062c5bedd4ab64ce59b43a1cf9920
PinkR6 <Alan000001233@hotmail.com>
2022-05-20 23:44:32 +1000

    Database connection

--- src/maze/datamanager/JDBCMazeDataSource.java
+++ src/maze/datamanager/JDBCMazeDataSource.java
@@ -19,36 +19,40 @@ public class JDBCMazeDataSource implements Data {
 
     public static final String CREATE_USER_TABLE =
             "CREATE TABLE IF NOT EXISTS users ("
-                 + "idx INTEGER PRIMARY KEY /* 40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
+                 + "id INTEGER PRIMARY KEY /* 40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
                  + "userName VARCHAR(30) , "
                  + "userPassword VARCHAR(30) , " + ");";
 
     public static final String CREATE_MAZE_TABLE =
             "CREATE TABLE IF NOT EXISTS mazes ("
-                 + "idx INTEGER PRIMARY KEY /* 40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
+                 + "id INTEGER PRIMARY KEY /* 40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
                  + "mazeName VARCHAR(30), "
                  + "mazeCreator VARCHAR(30) , "
                  + "mazeCreationTime VARCHAR(30) , "
                  + "mazeLastEdited (VARCHAR(30) , " + ");";
 
     private static final String INSERT_USER = "INSERT INTO users (userName, userPassword) VALUES (?, ?)";
-    private static final String GET_USERNAME = "SELECT userName FROM users";
-    private static final String GET_USERPASSWORD = "SELECT userPassword FROM users";
+    private static final String GET_USERID = "SELECT id FROM users WHERE userName=?";
+    private static final String GET_USERNAME = "SELECT userName FROM users WHERE userName=?";
+    private static final String GET_USERPASSWORD = "SELECT userPassword FROM users WHERE userName=?";
     private static final String DELETE_USER = "DELETE FROM users WHERE name=?";
     private static final String INSERT_MAZE = "INSERT INTO mazes (mazeName, mazeCreator, mazeCreationTime, mazeLastEdited) VALUES (?, ?, ?, ?)";
-    private static final String GET_MAZE = "SELECT * FROM mazes";
-    private static final String GET_MAZENAME = "SELECT mazeName FROM mazes";
-    private static final String GET_MAZECREATOR = "SELECT mazeCreator FROM mazes";
+    private static final String GET_MAZEID = "SELECT id FROM mazes WHERE name=?";
+    private static final String GET_MAZE = "SELECT * FROM mazes WHERE mazeName=?";
+    private static final String GET_MAZENAME = "SELECT mazeName FROM mazes WHERE mazeName=?";
+    private static final String GET_MAZECREATOR = "SELECT mazeCreator FROM mazes WHERE mazeName=?";
     private static final String GET_MAZECREATEDATE = "SELECT mazeCreationTime FROM mazes";
     private static final String GET_MAZEEDITDATE = "SELECT mazeLastEdited FROM mazes";
     private static final String DELETE_MAZE = "DELETE FROM maze WHERE mazeName =?";
 
     private Connection connection;
     private PreparedStatement addUser;
+    private PreparedStatement getUserID;
     private PreparedStatement getUsername;
     private PreparedStatement getPassword;
     private PreparedStatement deleteUser;
     private PreparedStatement addMaze;
+    private PreparedStatement getMazeID;
     private PreparedStatement getMaze;
     private PreparedStatement getMazeName;
     private PreparedStatement getMazeCreator;
@@ -64,11 +68,13 @@ public class JDBCMazeDataSource implements Data {
             st.execute(CREATE_MAZE_TABLE);
 
             addUser = connection.prepareStatement(INSERT_USER);
+            getUserID = connection.prepareStatement(GET_USERID);
             getUsername = connection.prepareStatement(GET_USERNAME);
             getPassword = connection.prepareStatement(GET_USERPASSWORD);
             deleteUser = connection.prepareStatement(DELETE_USER);
 
             addMaze = connection.prepareStatement(INSERT_MAZE);
+            getMazeID = connection.prepareStatement(GET_MAZEID);
             getMaze = connection.prepareStatement(GET_MAZE);
             getMazeName = connection.prepareStatement(GET_MAZENAME);
             getMazeCreator = connection.prepareStatement(GET_MAZECREATOR);
@@ -90,5 +96,12 @@ public class JDBCMazeDataSource implements Data {
         }
     }
 
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
 
