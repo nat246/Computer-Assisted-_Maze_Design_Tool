b60089e423d2884feac2b7ade61fb322e0778162
PinkR6 <Alan000001233@hotmail.com>
2022-05-20 16:35:43 +1000

    Database connection

--- src/maze/datamanager/JDBCMazeDataSource.java
+++ src/maze/datamanager/JDBCMazeDataSource.java
@@ -1,5 +1,7 @@
 package maze.datamanager;
 
+import org.mariadb.jdbc.MariaDbConnection;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,41 +15,73 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class JDBCMazeDataSource implements Data {
-    public static final String CREATE_TABLE =
-            "CREATE TABLE IF NOT EXISTS address ("
+
+    public static final String CERATE_TABLE =
+            "CREATE TABLE IF NOT EXISTS users ("
                  + "idx INTEGER PRIMARY KEY /* 40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
-                 + "Maze name VARCHAR(30), "
-                 + "Maze creator VARCHAR(30) , "
-                 + "Maze creation time VARCHAR(30) , "
-                 + "Maze last edited (VARCHAR(30) , " + ");";
+                 + "userName VARCHAR(30) , "
+                 + "userPassword VARCHAR(30) , " + ");";
 
-    private static final String INSERT_MAZE = "INSERT ";
+    public static final String CREATE_TABLE =
+            "CREATE TABLE IF NOT EXISTS mazes ("
+                 + "idx INTEGER PRIMARY KEY /* 40101 AUTO_INCREMENT */ NOT NULL UNIQUE,"
+                 + "mazeName VARCHAR(30), "
+                 + "mazeCreator VARCHAR(30) , "
+                 + "mazeCreationTime VARCHAR(30) , "
+                 + "mazeLastEdited (VARCHAR(30) , " + ");";
 
-    private static final String GET_MAZENAME = "";
+    private static final String INSERT_USER = "INSERT INTO users (userName, userPassword) VALUES (?, ?)";
 
-    private static final String GET_MAZECREATOR = "";
+    private static final String GET_USERNAME = "SELECT userName FROM users";
 
-    private static final String GET_MAZECREATEDATE = "";
+    private static final String GET_USERPASSWORD = "SELECT userPassword FROM users";
 
-    private static final String GET_MAZEEDITDATE = "";
+    private static final String DELETE_USER = "DELETE FROM users WHERE name=?";
 
-    private static final String COUNT_MAZES = "";
+    private static final String INSERT_MAZE = "INSERT INTO mazes (mazeName, mazeCreator, mazeCreationTime, mazeLastEdited) VALUES (?, ?, ?, ?)";
 
-    private static final String GET_USERNAME = "";
+    private static final String GET_MAZENAME = "SELECT mazeName FROM mazes";
 
-    private static final String GET_USER = "";
+    private static final String GET_MAZECREATOR = "SELECT mazeCreator FROM mazes";
 
-    private static final String DELETE_USER = "";
+    private static final String GET_MAZECREATEDATE = "SELECT mazeCreationTime FROM mazes";
 
-    private static final String COUNT_USERS = "";
+    private static final String GET_MAZEEDITDATE = "SELECT mazeLastEdited FROM mazes";
 
-
+    private static final String DELETE_MAZE = "DELETE FROM maze WHERE mazeName =?";
 
     private Connection connection;
 
     private PreparedStatement addUser;
 
+    private PreparedStatement getUsername;
 
+    private PreparedStatement getPassword;
 
+    private PreparedStatement deleteUser;
+
+    private PreparedStatement addMaze;
+
+    private PreparedStatement getMazeName;
+
+    private PreparedStatement getMazeCreator;
+
+    private PreparedStatement getMazeCreationDate;
+
+    private PreparedStatement getMazeLastEdited;
+
+    private PreparedStatement deleteMaze;
+
+    public JDBCMazeDataSource() {
+        connection = MazeDB.getInstance();
+        try {
+            Statement st = connection.createStatement();
+            st.execute(CERATE_TABLE);
+
+            addUser = connection.prepareStatement(INSERT_USER);
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
 
