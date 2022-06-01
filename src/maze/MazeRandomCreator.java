23c23ecbe90b4925def4ea1f434595ff8aec8f2d
Nathanael Ang <natnatang@hotmail.com>
2022-06-01 18:13:28 +1000

    added variables to MazeRandCreat

--- src/maze/MazeRandomCreator.java
+++ src/maze/MazeRandomCreator.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
  * used for creating a random generated maze
  */
 public class MazeRandomCreator {
-    private int mazeWidth;
-    private int mazeHeight;
+    private int mazeRow;
+    private int mazeCol;
     private int visitedCells;
     private ArrayList<ArrayList<Integer>> pathStack = new ArrayList<ArrayList<Integer>>();
 
