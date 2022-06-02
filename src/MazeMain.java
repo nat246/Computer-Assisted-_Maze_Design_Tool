f364184523fcc748e8a21aca31e500014bfc0763
Nathanael Ang <natnatang@hotmail.com>
2022-06-02 14:57:53 +1000

    testing branch

--- src/MazeMain.java
+++ src/MazeMain.java
@@ -5,7 +5,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class MazeMain {
 
-    // test
+    int test = 0;
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             try {
