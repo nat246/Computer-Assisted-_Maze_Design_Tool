fed62b44a81c94d6f525ff0034e2fd032dec01ed
Dedeepya <n10787101@qut.edu.au>
2022-06-12 20:39:52 +1000

    Updated Unit Testing for Grid Test class

--- src/maze/Test/GridTest.java
+++ src/maze/Test/GridTest.java
@@ -30,4 +30,13 @@ class GridTest {
     void testGetPos() throws MazeException {
         assertEquals(List.of(1,1), gridTest.getPos(),"The initial position of the grid is set for 1 x 1");
     }
+
+    @Test
+    /**
+     * Checks for wrong size of the grid when the original size of maze is 1x1 = 1
+     */
+    void WrongSize() {
+       assertEquals(10,gridTest.getSize(),"Size of the maze is incorrect");
+
+    }
 }
